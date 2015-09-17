4f8451d63b953adfee14a25ad8b6803dcb45a1c8
Miguel Alejandro <mvinan@icloud.com>
2015-09-17 09:32:23 -0500

    mode strict added

--- server.js
+++ server.js
@@ -1,34 +1,38 @@
-var express = require('express'),
-    path    = require('path'),
-    app     = express();
+(function(){
+  'use strict';
 
-var routes = require('./routes/index');
+  var express = require('express'),
+      path    = require('path'),
+      app     = express();
 
-var port = process.env.PORT || 3000;
+  var routes = require('./routes/index');
 
-app.set('view engine', 'jade');
-app.set('views', path.join(__dirname, 'views'));
+  var port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+  app.set('view engine', 'jade');
+  app.set('views', path.join(__dirname, 'views'));
 
-app.use('/', routes)
+  app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(function(req, res, next) {
-  var err = new Error('Parece que estas perdido.');
-  err.status = 404;
-  next(err);
-});
+  app.use('/', routes);
 
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
+  app.use(function(req, res, next) {
+    var err = new Error('Parece que estas perdido.');
+    err.status = 404;
+    next(err);
   });
-});
 
-app.listen(port, function () {
-  console.log('Escuchando en el puerto %s', port);
-})
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: {}
+    });
+  });
 
-module.exports = app;
+  app.listen(port, function () {
+    console.log('Escuchando en el puerto %s', port);
+  });
+
+  module.exports = app;
+})();
