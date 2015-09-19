662d968f327a3af2c1dda42aaf1a87d200001220
Miguel Alejandro <mvinan@icloud.com>
2015-09-19 11:25:42 -0500

    added comments on the source of the code

--- public/assets/js/form.js
+++ public/assets/js/form.js
@@ -1,3 +1,14 @@
+/**
+ * fullscreenForm.js v1.0.0
+ * http://www.codrops.com
+ *
+ * Licensed under the MIT license.
+ * http://www.opensource.org/licenses/mit-license.php
+ *
+ * Copyright 2014, Codrops
+ * http://www.codrops.com
+ */
+
 (function() {
   'use strict'
   var formWrap = document.getElementById( 'fs-form-wrap' );
